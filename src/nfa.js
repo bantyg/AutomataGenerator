9ec12246f6b8e4897d67b09de43fe7b704d1241d
Pooja Gurav <poojag@thoughtworks.com>
2016-06-06 19:23:14 +0530

    Added NFA tests.

--- src/nfa.js
+++ src/nfa.js
@@ -1,2 +1,15 @@
 var _ = require('lodash');
 
+var NFAGenerator = function (tuple) {
+    var stateMachine = [tuple.initialState];
+    return function (string) {
+        var next = tuple.transitionFun[tuple.initialState][string[0]];
+        string.split('').slice(1).forEach(function (alphabet) {
+            next = tuple.transitionFun[next][alphabet];
+            stateMachine.push(next);
+        });
+        return tuple.finalStates.indexOf(_.last(stateMachine)) != -1;
+    };
+};
+
+exports.NFAGenerator = NFAGenerator;
---
+++ tests/NFATest.js
@@ -0,0 +1,91 @@
+var assert = require('chai').assert;
+var NFAGenerator = require('../src/nfa.js').NFAGenerator;
+
+describe('Array', function() {
+    describe('All strings which has even number of zeros', function () {
+        var lang = {
+            states: ["a","b"],
+            transitionFun:{
+                a:{
+                    0:['a'],
+                    1:['a','b']
+                },
+                b:{
+                    0:[],
+                    1:[]
+                }
+            } ,
+            initialState:"a" ,
+            finalStates:['a']
+        };
+
+        var evenNoOfZeroDFA = NFAGenerator(lang);
+
+        it('DFA for string 01', function () {
+            assert.isTrue(evenNoOfZeroDFA('00'))
+        });
+        //
+        // it('DFA for string 0111', function () {
+        //     assert.isFalse(evenNoOfZeroDFA('0111'))
+        // });
+        //
+        // it('DFA for string 000', function () {
+        //     assert.isFalse(evenNoOfZeroDFA('000'))
+        // });
+        //
+        // it('DFA for string 00111010', function () {
+        //     assert.isTrue(evenNoOfZeroDFA('00111010'))
+        // });
+    });
+
+    // describe('All strings that begin with 1 and contain the string 001', function () {
+    //     var lang = {
+    //         states: ["a","b","c","d","e","f"],
+    //         transitionFun:{
+    //             a:{
+    //                 0:'f',
+    //                 1:'b'
+    //             },
+    //             b:{
+    //                 0:'c',
+    //                 1:'f'
+    //             },
+    //             c:{
+    //                 0:'d',
+    //                 1:'f'
+    //             },
+    //             d:{
+    //                 0:'d',
+    //                 1:'e'
+    //             },
+    //             e:{
+    //                 0:'e',
+    //                 1:'e'
+    //             },
+    //             f:{
+    //                 0:'f',
+    //                 1:'f'
+    //             }
+    //         } ,
+    //         initialState:"a" ,
+    //         finalStates:['e']
+    //     };
+    //
+    //     var startWith1Contains001DFA = DFAGenerator(lang)
+    //     it('DFA for string 1001', function () {
+    //         assert.isTrue(startWith1Contains001DFA('1001'))
+    //     });
+    //
+    //     it('DFA for string 0111', function () {
+    //         assert.isFalse(startWith1Contains001DFA('0111'))
+    //     });
+    //
+    //     it('DFA for string 000', function () {
+    //         assert.isFalse(startWith1Contains001DFA('000'))
+    //     });
+    //
+    //     it('DFA for string 100111010', function () {
+    //         assert.isTrue(startWith1Contains001DFA('100111010'))
+    //     });
+    // });
+});
