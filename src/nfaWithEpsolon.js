915e7aafbf5ac05e1e514ac9faf82ab472879797
Pooja Gurav <poojag@thoughtworks.com>
2016-06-08 18:39:40 +0530

    Handling the epsilon at first level

--- src/nfaWithEpsolon.js
+++ src/nfaWithEpsolon.js
@@ -3,11 +3,19 @@ var _ = require('lodash');
 var nfaWithEpsilon = function (tuple) {
     return function (string) {
         var initialState = tuple.initialState;
-
         var finalState = string.split('').reduce(function (prev,next) {
-            var states =  prev.map(function (state) {
-                return tuple.transitionFun[state][next];
-            });
+            var states = [];
+            if(tuple.transitionFun[prev[0]]["E"]){
+                states = tuple.transitionFun[prev[0]]["E"].map(function (state) {
+                    return tuple.transitionFun[state][next];
+                })
+            }
+            else {
+                states =  prev.map(function (state) {
+                    return tuple.transitionFun[state][next];
+                });
+
+            }
             return _.flatten(states);
         },[initialState]);
 
