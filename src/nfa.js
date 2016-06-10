a0fc2098c43e85c163269628f9011be78b9c9cab
Pooja Gurav <poojag@thoughtworks.com>
2016-06-10 10:03:34 +0530

    Using recursion for multiple epsolon

--- src/nfa.js
+++ src/nfa.js
@@ -1,27 +1,34 @@
 var _ = require('lodash');
 
-var NFAGenerator = function (tuple) {
-        return function (string) {
-            var initialState = tuple.initialState;
-            var finalState = string.split('').reduce(function (prev,next) {
-                var epsilonTransition = prev.map(function (state) {
-                    if(!tuple.transitionFun[state]){
-                        return state;
-                    }
-                    else if(tuple.transitionFun[state]["e"]){
-                        return tuple.transitionFun[state]["e"].map(function (state) {
-                            return tuple.transitionFun[state][next];
-                        })
-                    }
-                    else {
-                        return tuple.transitionFun[state][next];
-                    }
-                });
-                return _.flattenDeep(epsilonTransition);
-            },[initialState]);
-            return _.intersection(tuple.finalStates, finalState).length > 0;
+var recursion = function (tuple,alphabet,state) {
+    var states = [];
+    if(!tuple.transitionFun[state]) return [];
+    if(tuple.transitionFun[state]['e']){
+        var epsilon = tuple.transitionFun[state]['e'].map(function (subState) {
+            if (tuple.transitionFun[subState]['e']) {
+                return recursion(tuple, alphabet, subState);
+            }
+            else return tuple.transitionFun[subState][alphabet] ? tuple.transitionFun[subState][alphabet] : subState;
+        });
+        states = states.concat(epsilon);
+    }
+    if(tuple.transitionFun[state][alphabet]){
+        states = states.concat(tuple.transitionFun[state][alphabet]);
+    }
+    return states;
+};
 
-        };
+var NFAGenerator = function (tuple) {
+    return function (string) {
+        var initialState = tuple.initialState;
+        var finalState = string.split('').reduce(function (states,alphabet) {
+            var subStates =  states.map(function (state) {
+                return recursion(tuple,alphabet,state)
+            });
+            return _.flattenDeep(subStates);
+        },[initialState]);
+        return _.intersection(tuple.finalStates, finalState).length > 0;
+    };
 };
 
 exports.NFAGenerator = NFAGenerator;
--- tests/NFATest.js
+++ tests/NFATest.js
@@ -104,4 +104,59 @@ describe('Array', function() {
         });
     });
 
+    describe("NFA for languages with epsilon", function(){
+        var language = {
+            "states" : ["q1", "q2", "q3", "q4", "q5", "q6"],
+            "alphabets" : ["1", "0"],
+            "transitionFun" : {
+                "q1": {"e": ["q2","q3"]},
+                "q2": {"1": ["q2"], "0":["q4"]},
+                "q3": {"0": ["q3"], "1":["q5"]},
+                "q4": {"0": ["q2"], "1":["q2"]},
+                "q5": {"0": ["q5"],"1": ["q6"]},
+                "q6": {"0": ["q6"]}
+            },
+            "initialState" : "q1" ,
+            "finalStates" : ["q2", "q6"]
+        };
+
+        var nfa_for_strings_contains_exactly_two_1s_or_even_0s = NFAGenerator(language);
+        it("should return true for 11", function(){
+            assert.isTrue(nfa_for_strings_contains_exactly_two_1s_or_even_0s("11"));
+        });
+        it("should return true for 00", function(){
+            assert.isTrue(nfa_for_strings_contains_exactly_two_1s_or_even_0s("00"));
+        });
+        it("should return true for 1001", function(){
+            assert.isTrue(nfa_for_strings_contains_exactly_two_1s_or_even_0s("1001"));
+        });
+    });
+
+    describe("NFA for languages with epsilon...", function(){
+        it("should accept string with tuple_two_or_three_length with multiple epsilon transition",function(){
+            var tuple_two_or_three_length = {
+                transitionFun:{
+                    "q1":{"e":["q2","q5"]},
+                    "q2":{"0":["q3"]},
+                    "q3":{"0":["q4"]},
+                    "q5":{"0":["q6"]},
+                    "q6":{"0":["q7"]},
+                    "q7":{"e":["q8"]},
+                    "q8":{"e":["q9"]},
+                    "q9":{"0":["q4"]}
+                },
+                alphabets:["0"],
+                states:["q1","q2","q3","q4","q5","q6","q7","q8","q9"],
+                initialState:"q1",
+                finalStates:["q4"]
+            };
+
+            var nfa_two_or_three_length = NFAGenerator(tuple_two_or_three_length);
+            // assert.isFalse(nfa_two_or_three_length("0"));
+            // assert.isTrue(nfa_two_or_three_length("00"));
+            assert.isTrue(nfa_two_or_three_length("000"));
+
+        })
+    });
+
 });
